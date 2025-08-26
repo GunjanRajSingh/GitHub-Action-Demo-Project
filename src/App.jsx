6ea77dd643b08a519d7a3b8d01cd5232110c710d
GunjanRajSingh <52822461+GunjanRajSingh@users.noreply.github.com>
2025-08-26 13:54:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
     <>
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects by Gunjan</h2>
-      <h3>Created by AAG</h3>
-      <h4>Created by Gunjan Raj Singh</h4>
+      <h3>Created by Gunjan Raj Singh</h3>
     </>
   )
 }
